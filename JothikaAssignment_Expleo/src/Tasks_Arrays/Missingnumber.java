a56a805a743c6451da2a751c1b0f68cfdc8d823e
Jothika <raviravi31799@gmail.com>
2026-03-20 11:17:55 +0530

    Arrays handson

--- JothikaAssignment_Expleo/src/Tasks_Arrays/Missingnumber.java
+++ JothikaAssignment_Expleo/src/Tasks_Arrays/Missingnumber.java
@@ -1,27 +1,31 @@
 package Tasks_Arrays;
+
 import java.util.*;
 import java.util.Arrays;
+
 public class Missingnumber {
 
 	public static void main(String[] args) {
-		Scanner sc= new Scanner (System.in);
-	int missing=0;
-	int []arr=new int [99];
-		for(int i=0;i<arr.length;i++) {
-			arr[i]=sc.nextInt();
-			
-			}
-		for(int i=0;i<arr.length;i++) {
-			arr[i]=sc.nextInt();
-			if(arr[i]!=i+1) {
-					missing =i+1;
-				}
-			}
-		System.out.println("Missing: " +missing);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter size:");
+		int size = sc.nextInt();
+		System.out.println("Enter array elements:");
+		int[] arr = new int[size];
+		for (int i = 0; i < arr.length-1; i++) {
+			arr[i] = sc.nextInt();
+
 		}
-	
-		
+		Arrays.sort(arr);
+		String missing = " ";
+
+		for (int i = 0; i < arr.length-1; i++) {
+			for (int j = arr[i] + 1; j < arr[i + 1]; j++) {
+				missing += j + " ";
+			}
+
+		}
+		System.out.println("Missing number:" + missing);
 
 	}
 
-
+}
